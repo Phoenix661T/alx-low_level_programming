c05b3a5a55a252124068d04694709b2b76ea4fab
Phoenix661T <thabangmaloka661@outlook.com>
2021-09-09 10:55:52 -0700

    infinite add

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,60 +1,46 @@
 #include "main.h"
-
 /**
- * infinite_add - C function that adds two numbers stored
- *in strings to a buffer.
- *Assumes that strings are never empty and
- *that numbers will always be positive, or 0.
- *Assumes there are only digits stored in the number strings.
- *If result can be stored in the buffer,
- *returns a pointer to the result.
- *If result cannot be stored in the buffer, returns `0`.
- *@n1:first number to be added
- *@n2:second number to be added
- *@r: store result
- *@size_r: size of buffer
- *Return:returns pointer to result
- */
-
+ * infinite_add - adds two numbers
+ * @n1: number1
+ * @n2: number2
+ * @r: result
+ * @size_r: size result
+ * Return: r addition
+ **/
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-}
+int i = 0, j = 0, a, b, c, n, aux, dec = 0;
 
-/**
- * add_strings - Adds the numbers stored in two strings.
- * @n1: The string containing the first number to be added.
- * @n2: The string containing the second number to be added.
- * @r: The buffer to store the result.
- * @r_index: The current index of the buffer.
- *
- * Return: If r can store the sum - a pointer to the result.
- *         If r cannot store the sum - 0.
- */
-
-char *add_strings(char *n1, char *n2, char *r, int r_index)
+while (n1[i] != '\0')
 {
-int num, tens = 0;
-
-for (; *n1 && *n2; n1--, n2--, r_index--)
-{
-num = (*n1 - '0') + (*n2 - '0');
-num += tens;
-*(r + r_index) = (num % 10) + '0';
-tens = num / 10;
+i++;
 }
-
-for (; *n1; n1--; r_index++)
+while (n2[j] != '\0')
 {
-num = *(n1 - '0') + tens;
-*(r + r_index) = (num % 10) + '0';
-tens = num / 10;
+j++;
 }
-
-for (; *n2; n2--;  r_index--)
+for (n = 0; n < j || n < i; n++)
 {
-num = (*n2 - '0') + tens;
-*(r + r_index) = (num % 10) + '0';
-tens = num / 10;
+a = (i - n) > 0 ? (n1[i - n - 1] - '0') : 0;
+b = (j - n) > 0 ? (n2[j - n - 1] - '0') : 0;
+c = a + b + dec;
+r[n] = (c % 10) + '0';
+dec = c > 9 ? 1 : 0;
 }
-
+if (dec == 1)
+{ r[n] = '1';
+r[n + 1] = '\0'; }
+else
+{ r[n] = '\0';
+n--; }
+for (i = 0; i < n + 1; i++)
+{
+for (j = 0; j < (n - i); j++)
+{
+aux = r[j + 1];
+r[j + 1] = r[j];
+r[j] = aux;
+}
+}
+return (n < size_r - 1 ? r : 0);
 }
