0e7a2fe7586895d61e8d7e755ecb175a146a1946
Phoenix661T <thabangmaloka661@outlook.com>
2021-09-15 13:28:20 -0700

    wildcmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,59 +1,58 @@
 #include "main.h"
 
-int wildcmp_local(char *s1, char *s2, int i, int j, int wildIndex, int isWild);
 /**
- * wildcmp - compare two strings
- * @s1: the first string
- * @s2: the second string
- *
- * Return: 1 if equal and 0 otherwise
+ * wildcmp - compare two strings with "wildcard expansion" capabilities
+ * @s1: string 1
+ * @s2: string 2
+ * Return: 1 if strings can be considered identical, else 0
  */
+
 int wildcmp(char *s1, char *s2)
 {
-return (wildcmp_local(s1, s2, 0, 0, -1, 0));
-}
-
-
-/**
- * wildcmp_local - compare two strings
- * @s1: the first string
- * @s2: the second string
- * @i: the first string current index
- * @j: the second string current index
- * @wildIndex: the index of the wild card
- * @isWild: wild card status for the second string
- *
- * Return: 1 if equal and 0 otherwise
- */
-int wildcmp_local(char *s1, char *s2, int i, int j, int wildIndex, int isWild)
-{
-if (s1[i] == '\0')
-{
-if (s2[j] == '\0')
+if (*s1 == '\0' && *s2 == '\0')
 return (1);
-if (s2[j] == '*')
-return (wildcmp_local(s1, s2, i, j + 1, j, 1));
+else if (*s1 == '\0' || *s2 == '\0')
+{
+if (*s1 == '\0' && *s2 == '*')
+return wildcmp(s1, ++s2);
+else if (*s1 == '*' && *s2 == '\0')
+return wildcmp(++s1, s2);
 return (0);
 }
 
-if (isWild)
+if (*s1 == *s2)
 {
-if (s2[j] == '\0')
-return (1);
-else if (s2[j] == '*')
-return (wildcmp_local(s1, s2, i, j + 1, j, 1));
-else if (s1[i] == s2[j])
-return (wildcmp_local(s1, s2, i + 1, j + 1, wildIndex, 0));
+return wildcmp(++s1, ++s2);
+}
+else if (*s1 == '*')
+{
+if (*(s1 + 1) == '*')
+return wildcmp(++s1, s2);
 else
-return (wildcmp_local(s1, s2, i + 1, j, wildIndex, 1));
+{
+return wildcmp(s1, findsrc(s2, *(s1 + 1), 0, 0) + s2);
+}
+}
+else if (*s2 == '*')
+{
+if (*(s2 + 1) == '*')
+return wildcmp(s1, ++s2);
+else
+{
+return wildcmp(s1 + findsrc(s1, *(s2 + 1), 0, 0), s2);
+}
 }
 
-if (s2[j] == '\0')
 return (0);
 
-if (s2[j] == '*')
-return (wildcmp_local(s1, s2, i, j + 1, j, 1));
-else if (s1[i] == s2[j])
-return (wildcmp_local(s1, s2, i + 1, j + 1, wildIndex, 0));
-return (wildcmp_local(s1, s2, i + 1, wildIndex + 1, wildIndex, 1));
+}
+
+int findsrc(char *s, char c, int i, int p)
+{
+if (*(s + i) == '\0')
+return (p + 1);
+else if (*(s + i) == c || *(s + i) == '*')
+p = i;
+
+return (findsrc(s, c, i + 1, p));
 }
