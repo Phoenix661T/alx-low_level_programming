f61adce502536da52bf0e0172b469aa72161e7bb
Phoenix661T <thabangmaloka661@outlook.com>
2021-09-23 10:23:06 -0700

    string nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,48 +1,38 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
+#include <stddef.h>
 
 /**
- *string_nconcat - concat 2 string
- *@s1: string 1
- *@s2: string 2
- *@n: integer value
- *Return: pointer points to a new allocated space else NULL
+ * string_nconcat - concatenates 2 strings, takes the first n chars of s2
+ * @s1: string 1
+ * @s2: string 2
+ * @n: amount of chars to be used from s2
+ *
+ * Return: pointer to the new string
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-char *str;
-unsigned int s1_len, s2_len, i;
+char *new_string;
+int i = 0, j = 0, size_1 = strlen(s1), size_2;
 
-
-if (s1 == NULL)
-s1 = "";
-if (s2 == NULL)
-s2 = "";
-
-for (s1_len = 0; s1[s1_len] != '\0'; s1_len++)
-;
-
-for (s2_len = 0; s2[s2_len] != '\0'; s2_len++)
-;
-
-str = malloc(s1_len + s2_len + 1);
-
-
-if (n >= s2_len)
-n = s2_len;
-
-for (i = 0; s1[i] != '\0'; i++)
-str[i] = s1[i];
-
-for (i = 0; i < n; i++)
-str[s1_len + i] = s2[i];
-
-str[s1_len + n] = '\0';
-
-if (str == NULL)
+n < (unsigned int) strlen(s2) ? (size_2 = n) : (size_2 = strlen(s2));
+new_string = (char *) malloc(size_1 + size_2 + 1);
+if (new_string != NULL)
+{
+for (; i < size_1; i++)
+{
+new_string[i] = s1[i];
+}
+for (; j < size_2; j++)
+{
+new_string[i + j] = s2[j];
+}
+new_string[i + j] = '\0';
+}
+else
+{
 return (NULL);
+}
 
-return (str);
+return (new_string);
 }
