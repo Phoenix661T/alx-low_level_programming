6c0d6cda9753e42c7754353549603e4e743671d8
Phoenix661T <thabangmaloka661@outlook.com>
2021-09-23 09:24:47 -0700

    string nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,51 +1,42 @@
 #include "main.h"
-#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * string_nconcat - concatenate 2 strings, only n bytes of s2
- * @s1: string 1
- * @s2: string 2
- * @n: bytes to include of s2
- * Return: NULL if fail, else pointer to malloc memory
+ * string_nconcat - concatenates two strings.
+ * @s1: first string to copy
+ * @s2: second string to copy
+ * @n: number of bytes of s2 to copy
+ *
+ * Return: char pointer to newly allocated place in memory
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-char *p;
-int strlen1, i, c;
+unsigned int i, j, k;
+char *s;
 
 if (s1 == NULL)
-s1 = "";
-if (s2 == NULL)
-s2 = "";
-
-strlen1 = (unsigned int)_strlen(s1);
-p = malloc((strlen1 + n + 1) * sizeof(char));
-if (p == NULL)
-return (NULL);
-for (i = 0, c = 0; i < (strlen1 + n); i++)
-{
-if (i < strlen1)
-p[i] = s1[i];
+i = 0;
 else
-p[i] = s2[c++];
-}
-p[i] = '\0';
-
-return (p);
-}
-
-/**
- * _strlen - find length of string
- * @s: string
- * Return: length of string
- */
-
-int _strlen(char *s)
 {
-int i;
-
-for (i = 0; s[i] != '\0'; i++)
+for (i = 0; s1[i]; i++)
 ;
-return (i);
+}
+if (s2 == NULL)
+j = 0;
+else
+{
+for (j = 0; s2[j]; j++)
+;
+}
+if (j > n)
+j = n;
+s = malloc(sizeof(char) * (i + j + 1));
+if (s == NULL)
+return (NULL);
+for (k = 0; k < i; k++)
+s[k] = s1[k];
+for (k = 0; k < j; k++)
+s[k + i] = s2[k];
+s[i + j] = '\0';
+return (s);
 }
