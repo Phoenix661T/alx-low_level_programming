5bc5bebaf4b26675eedab7f3ca0184a9b03b9acd
Phoenix661T <thabangmaloka661@outlook.com>
2021-09-27 03:26:21 -0700

    init dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,31 @@
+#ifndef FILE_DOG_H
+#define FILE_DOG_H
+
+/**
+ * struct dog - new type struct
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ * Description: new type struct dog with the above elements
+ * Reyurn: Always 0
+ */
+
+typedef struct dog
+{
+  char *name;
+  float age;
+  char *owner;
+};
+
+/**
+ * dog_t - typedef for struct dog
+ */
+
+struct dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif /* FILE_DOG_H */
