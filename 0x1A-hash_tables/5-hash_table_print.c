16a6d60f2a2f2c57647d060fe3ae2126bcbedbe0
Phoenix661T <thabangmaloka661@outlook.com>
2021-12-16 02:52:19 -0800

    0x1A-hash_tables

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - prints a hash table.
+ * @ht: Hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+unsigned long int i = 0, j = 0;
+hash_node_t *tmp = NULL;
+
+if (!ht)
+return;
+putchar('{');
+while (i < ht->size)
+{
+tmp = ht->array[i];
+
+while (tmp)
+{
+if (j != 0)
+printf(", ");
+printf("'%s': '%s'", tmp->key, tmp->value);
+tmp = tmp->next;
+j++;
+}
+i++;
+}
+printf("}\n");
+
+}
